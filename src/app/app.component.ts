9f4ca4d7e06a0654ae3e0c098224479c63a9152b
benjamin00 <[benjaminhill00@gmail.com]>
2019-02-16 13:07:00 -0800

    added working files

---
+++ src/app/app.component.ts
@@ -0,0 +1,17 @@
+import { Component } from '@angular/core';
+import { AngularFirestore } from '@angular/fire/firestore';
+import { Observable } from 'rxjs';
+
+@Component({
+  selector: 'app-root',
+  templateUrl: 'app.component.html',
+  styleUrls: ['app.component.css']
+})
+export class AppComponent {
+  items: Observable<any[]>;
+  names: Observable<any[]>;
+  constructor(db: AngularFirestore) {
+    this.items = db.collection('test').valueChanges();
+    this.names = db.collection('IssueNames').valueChanges();
+  }
+}
---
+++ src/environments/environment.ts
@@ -0,0 +1,24 @@
+// This file can be replaced during build by using the `fileReplacements` array.
+// `ng build --prod` replaces `environment.ts` with `environment.prod.ts`.
+// The list of file replacements can be found in `angular.json`.
+
+export const environment = {
+  production: false,
+ firebase: {
+    apiKey: "AIzaSyAqGU1kG78wZcuiqR5AL5m6i8pd6dJ8h9g",
+    authDomain: "stepwise-60ca4.firebaseapp.com",
+    databaseURL: "https://stepwise-60ca4.firebaseio.com",
+    projectId: "stepwise-60ca4",
+    storageBucket: "stepwise-60ca4.appspot.com",
+    messagingSenderId: "75429022585"
+  }
+};
+
+/*
+ * For easier debugging in development mode, you can import the following file
+ * to ignore zone related error stack frames such as `zone.run`, `zoneDelegate.invokeTask`.
+ *
+ * This import should be commented out in production mode because it will have a negative impact
+ * on performance if an error is thrown.
+ */
+// import 'zone.js/dist/zone-error';  // Included with Angular CLI.
