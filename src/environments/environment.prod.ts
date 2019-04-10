a18f5530be17a6046cc23ba9652c19aa1e162cec
benjamin00 <[benjaminhill00@gmail.com]>
2019-04-09 22:35:22 -0500

    added firebase to production environment

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,3 +1,11 @@
 export const environment = {
-  production: true
+  production: true,
+  firebase: {
+    apiKey: "AIzaSyAqGU1kG78wZcuiqR5AL5m6i8pd6dJ8h9g",
+    authDomain: "stepwise-60ca4.firebaseapp.com",
+    databaseURL: "https://stepwise-60ca4.firebaseio.com",
+    projectId: "stepwise-60ca4",
+    storageBucket: "stepwise-60ca4.appspot.com",
+    messagingSenderId: "75429022585"
+  }
 };
